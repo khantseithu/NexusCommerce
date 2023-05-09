e651d4ba4ed8c14efbaa0b879506bc009ce289b1
Khant Sithu <khantseithuu@gmail.com>
2023-05-09 16:25:38 +0000

    fix bug in remove from cart

--- store.ts
+++ store.ts
@@ -7,6 +7,7 @@ type CartState = {
   cart: AddCartType[];
   toggleCart: () => void;
   addToCart: (item: AddCartType) => void;
+  removeFromCart: (item: AddCartType) => void;
 };
 
 export const useCartStore = create<CartState>()(
@@ -28,8 +29,24 @@ export const useCartStore = create<CartState>()(
               ),
             };
           }
-
           return { cart: [...state.cart, { ...item, quantity: 1 }] };
+        }),
+      removeFromCart: (item) =>
+        set((state) => {
+          // check if item exists in cart and quantity is greater than 1 then decrement
+          const itemExists = state.cart.find((i) => i.id === item.id);
+          if (itemExists && itemExists.quantity! > 1) {
+            const updatedCart = state.cart.map((i) =>
+              i.id === item.id
+                ? { ...i, quantity: (itemExists.quantity as number) - 1 }
+                : i
+            );
+            return { cart: updatedCart };
+          } else {
+            // remove item from cart
+            const updatedCart = state.cart.filter((i) => i.id !== item.id);
+            return { cart: updatedCart };
+          }
         }),
     }),
     { name: "cart-store" }
